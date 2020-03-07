b71e976c0e4dadb0688ecaafe069026f888c13ef
clockwerkz <clockwerkz@gmail.com>
2020-03-06 23:15:43 -0800

    adds google oauth via passportja

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res)=> {
-    res.send({msg: 'Greetings'});
-});
+
+
+//redirect URI field: http://localhost:5000/auth/google/callback
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.listen(PORT, ()=> {
