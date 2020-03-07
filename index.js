56c216838e0371d414ffc40a828ba20ee21bc4ef
clockwerkz <clockwerkz@gmail.com>
2020-03-06 23:34:14 -0800

    adding google oauth

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const PORT = process.env.PORT || 5000;
 
 
-//redirect URI field: http://localhost:5000/auth/google/callback
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: keys.clientID,
+    clientSecret: keys.clientSecret,
     callbackURL: "http://www.example.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
