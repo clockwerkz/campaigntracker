4204c54e55755a18fc9f844cef0c9c29edd2ed24
clockwerkz <clockwerkz@gmail.com>
2020-03-12 23:40:56 -0700

    adds done function to passport cb

--- services/passport.js
+++ services/passport.js
@@ -11,19 +11,19 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
-    let user = await User.findOne({ googleID : profile.id });
     try {
+      let user = await User.findOne({ googleID : profile.id });
       if (!user) {
         console.log("user does not exist");
         user = await User({ googleID : profile.id }).save();
       } else {
         console.log("User already existed");
       }
-      console.log(user);
-  } catch (err) {
-    console.log("Error accessing user info", err);
-  }
-    done();
+      done(null, user);
+    } catch (err) {
+      console.log("Error accessing user info", err);
+    }
+      
   }
 ));
 
