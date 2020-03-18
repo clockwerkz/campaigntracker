db33c281483ad1ea85f1564af3b7ae84928daad4
clockwerkz <clockwerkz@gmail.com>
2020-03-17 23:18:56 -0700

    adds logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get('/google/callback', passport.authenticate('google'));
 
 
-router.get('/api/current_user', (req, res)=> {
+router.get('/logout', (req,res)=> {
+    req.logout();
+    res.send({msg: "You are now logged out", user: req.user});
+});
+
+
+router.get('/current_user', (req, res)=> {
     res.send(req.user);
 });
 
